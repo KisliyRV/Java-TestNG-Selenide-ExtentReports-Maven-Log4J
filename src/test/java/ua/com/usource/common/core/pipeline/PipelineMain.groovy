6b2d8e7de29ab9a76533e5d875434dba95e47645
Zhuravl <ilya.zhuravskiy@gmail.com>
2024-06-11 13:31:19 +0300

    Removed Slack messaging

--- src/test/java/ua/com/usource/common/core/pipeline/PipelineMain.groovy
+++ src/test/java/ua/com/usource/common/core/pipeline/PipelineMain.groovy
@@ -1,18 +1,12 @@
 package ua.com.usource.common.core.pipeline
 
-library identifier: "shared-library@$BRANCH",
-        retriever: modernSCM([
-                $class       : 'GitSCMSource',
-                credentialsId: '{REPLACE_WITH_CREDENTIALS}',
-                remote       : 'git@github.com:Zhuravl/Java-TestNG-Selenide-ExtentReports-Maven-Log4J.git'])
-
 pipeline {
     agent any
 
     stages {
         stage("Start regression") {
             steps {
-                slackSend message: "The Regression has been started!", channel: 'qualityassurance', tokenCredentialId: '{REPLACE_WITH_CREDENTIALS}'
+                echo "The Regression has been started!"
                 testsWithRerun(['TARGET_URL'     : TARGET_URL,
                                 'BROWSER_NAME'   : BROWSER_NAME,
                                 'BROWSER_VERSION': BROWSER_VERSION,
@@ -25,7 +19,7 @@ pipeline {
     }
     post {
         always {
-            slackSend message: "The Regression has been completed!\nSee results here: $RUN_DISPLAY_URL", channel: 'qualityassurance', tokenCredentialId: '{REPLACE_WITH_CREDENTIALS}'
+            echo "The Regression has been completed!\nSee results here: $RUN_DISPLAY_URL"
         }
     }
 }
