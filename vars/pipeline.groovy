3f3125e1a653352c1db8700fa9065168bfea72aa
Zhuravl <ilya.zhuravskiy@gmail.com>
2024-06-12 19:31:52 +0300

    Updated the pipeline logic 25

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -20,16 +20,17 @@ node {
             stage("Execution #${run}") {
                 try {
                     sh "mvn clean test site -Durl=${TARGET_URL} -Dbrowser=${BROWSER_NAME} -Dversion=${BROWSER_VERSION} -Dtest=${scope} -Dtimeout=${TIMEOUT} -DthreadCount=${threads}"
-                } finally {
-                    scope = getFailedTests()
-                    if (scope == null || scope.isEmpty()) {
-                        //All tests passed - moving to the next stage
-                        sh 'exit 0'
-                    } else {
-                        //Some tests failed - reducing the number of threads and re-running with the updated scope (the failed tests only)
-                        threads = reduceThreads(threads)
-                    }
+                } catch (Exception ignored) {
+                    //Do nothing here and just move forward
                 }
+            }
+            scope = getFailedTests()
+            if (scope == null || scope.isEmpty()) {
+                //All tests passed - moving to the next stage
+                return
+            } else {
+                //Some tests failed - reducing the number of threads and re-running with the updated scope (the failed tests only)
+                threads = reduceThreads(threads)
             }
         }
     }
